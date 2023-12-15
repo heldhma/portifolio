da10ce91c3570049957bfeda3f30bd3f945db6af
heldhma <132068594+heldhma@users.noreply.github.com>
2023-12-14 21:36:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('typewriter').style.animation = 'apagar 3s steps(30, end) infinite';
+    }, 3000);
+});
