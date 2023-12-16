0d6d5bb4399c63205a442c1431a28c0ea66e9b86
heldhma <132068594+heldhma@users.noreply.github.com>
2023-12-16 07:46:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,36 @@ document.addEventListener('DOMContentLoaded', function () {
     const imagem2 = document.getElementById('imagem2');
     const images = document.querySelectorAll('.imagem2 img');
 
-    let counter = 0;
+    let counter = 1;
     const size = images[0].clientWidth;
 
+    const firstClone = images[0].cloneNode(true);
+    const lastClone = images[images.length - 1].cloneNode(true);
+
+    imagem2.appendChild(firstClone);
+    imagem2.insertBefore(lastClone, images[0]);
+
     function nextSlide() {
-        if (counter >= images.length - 1) return;
+        if (counter >= images.length) {
+            counter = 1;
+            imagem2.style.transition = 'none';
+            imagem2.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        }
         counter++;
         imagem2.style.transition = 'transform 0.5s ease-in-out';
         imagem2.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 
     function prevSlide() {
-        if (counter <= 0) return;
+        if (counter <= 0) {
+            counter = images.length - 2;
+            imagem2.style.transition = 'none';
+            imagem2.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        }
         counter--;
         imagem2.style.transition = 'transform 0.5s ease-in-out';
         imagem2.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 
-    setInterval(nextSlide, 3000); // Troca de slide a cada 3 segundos
+    setInterval(nextSlide, 3500);
 });
